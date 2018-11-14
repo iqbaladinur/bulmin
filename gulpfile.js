c61eb2595ce94bb7260e1cc66a6263ab513e560c
iqbaladinur <iqbaladinur@gmail.com>
2018-11-14 09:15:13 +0700

    update gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,31 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const minify = require("gulp-csso");
-const concat = require("gulp-concat");
-const sourcemaps = require('gulp-sourcemaps');
+const rename = require("gulp-rename");
 const wait = require('gulp-wait');
-gulp.task('css', function () {
+gulp.task('css-minified', function () {
     return gulp.src('src/scss/*.scss')
         .pipe(wait(1500))
         .pipe(sass().on('error', sass.logError))
         .pipe(minify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('css', function () {
+    return gulp.src('src/scss/*.scss')
+        .pipe(wait(1500))
+        .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('build/css'));
 });
 
 gulp.task('js', function () {
     return gulp.src('src/js/**/*.js')
-        .pipe(sourcemaps.init())
-        .pipe(concat('bulmin.min.js'))
-        .pipe(sourcemaps.write())
         .pipe(gulp.dest('build/js'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch(["src/scss/**/*.scss", "src/js/**/*.js"], ["css", "js"]);
+    gulp.watch(["src/scss/**/*.scss", "src/js/**/*.js"], ["css-minified", "css", "js"]);
 })
 gulp.task('build', ["css", "js"]);
 gulp.task("default", ["watch"]);
